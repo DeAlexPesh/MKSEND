bebcadc2c4951f36d382cd3663bce5516bd13445
Пешков Александр Юрьевич <a.peshkov@ente-ltd.ru>
2020-07-20 19:09:27 +0500

    0

--- MKSEND.py
+++ MKSEND.py
@@ -3,45 +3,83 @@ import re
 import socket
 import sys
 import os
+import requests
+import time
 
-sg.ChangeLookAndFeel('LightGreen')
+sg.ChangeLookAndFeel('LightGrey3')
 
 ip = '192.168.5.254'
 httpPort = 80
-tcpPort = 8080
-gcode = ''
-cmnd = ''
+sockPort = 8080
+inGcode = ''
+inCmnd = ''
 
 
-def rawSocketSend():
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
-            conn.connect((ip, tcpPort))
-            conn.sendall(cmnd)
-            while 1:
-                reply = ''
-                data = ''
-                while data != b'\n':
-                    data = conn.recv(1)
-                    reply = reply + str(data, 'utf-8')
-
-                reply = reply.replace("\n", "").replace("\r", "")
-                print('Received:', repr(reply))
-
-    except socket.error as exc:
-        print("Caught exception socket.error : %s" + exc)
-    finally:
-        conn.close()
+def echo(s, isNew=False):
+    if isNew:
+        window['_OUTPUT_'].update(s)
+    else:
+        window['_OUTPUT_'].print(s)
     return
 
 
+def sendRawSocket(i, isLog=False):
+    try:
+        cmnd = str.encode(i + '\r\n')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
+            conn.connect((ip, sockPort))
+            conn.sendall(cmnd)
+            reply = ''
+            while reply != 'ok':
+                reply = ''
+                data = b''
+                while data != b'\n':
+                    data = conn.recv(1)
+                    reply = reply + str(data, 'utf-8')
+                reply = reply.replace("\n", "").replace("\r", "")
+                if isLog:
+                    echo(reply)
+        return True
+    except Exception as ex:
+        echo(f'Exception: {ex}')
+        return False
+    finally:
+        conn.close()
+        conn = None
+
+
+def sendFile(fileUrl):
+    try:
+        with open(fileUrl, 'rb') as f:
+            fileData = f.read()
+        fileName = os.path.basename(fileUrl)
+        url = f'http://{ip}/upload?X-Filename={fileName}'
+        res = requests.post(url=url,
+                            data=fileData,
+                            headers={'Content-Type': 'application/octet-stream'})
+        return False if res.status_code != 200 else True
+    except Exception as ex:
+        echo(f'Exception: {ex}')
+        return False
+
+
 layout = [
-    [sg.Text('IP:', size=(7, 1), justification='right'), sg.In(key='_IP_', size=(
-        15, 1), change_submits=True, do_not_clear=True, default_text=ip)],
-    [sg.Text('GCODE:', size=(7, 1), justification='right'), sg.In(key='_GCODE_', size=(
-        25, 1), change_submits=True, do_not_clear=True, readonly=True), sg.FileBrowse('Обзор', size=(10, 1)), sg.Submit('Печать', key='_PRINT_', size=(10, 1))],
-    [sg.Text('CMND:', size=(7, 1), justification='right'), sg.In(key='_CMND_', size=(
-        25, 1), change_submits=True, do_not_clear=True), sg.Submit('Отправить', key='_SEND_', size=(10, 1))]
+    [sg.Text('IP:', size=(7, 1), justification='right'),
+     sg.InputText(key='_IP_', size=(25, 1), background_color='white', justification='center',
+                  change_submits=True, do_not_clear=True, default_text=ip)],
+    [sg.Text('CMND:', size=(7, 1), justification='right'),
+     sg.InputText(key='_CMND_', size=(25, 1), justification='center',
+                  change_submits=True, do_not_clear=True),
+     sg.Submit('Отправить', key='_SEND_', size=(15, 1))],
+    [sg.Text('GCODE:', size=(7, 1), justification='right'),
+     sg.InputText(key='_GCODE_', size=(25, 1), change_submits=True,
+                  do_not_clear=True, readonly=True),
+     sg.FileBrowse('...', size=(3, 1)),
+     sg.Submit('Печать', key='_PRINT_', size=(10, 1))],
+    [sg.Text('ECHO:', size=(7, 10), justification='right'),
+     sg.Multiline(key='_OUTPUT_', size=(42, 10), disabled=True)],
+    [sg.Text('', size=(7, 1), justification='right'), sg.ProgressBar(
+        100, key='_PROGRESS_', size=(29, 20), orientation='h')],
 ]
 window = sg.Window('MKSEND', layout)
 
@@ -57,44 +95,47 @@ while True:
             r"(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)", ipEntered)
         if isIp:
             ip = ipEntered
-            print('ip')
+            window.Element(event).update(background_color='white')
         else:
             ip = ''
-
-        # window[event].update(ip_entered)
+            window.Element(event).update(background_color='#ff5555')
 
     if event in ('_GCODE_'):
-        gcode = values[event]
-        print(gcode)
+        inGcode = values[event]
 
     if event in ('_CMND_'):
-        cmnd = str.encode(values[event] + '\r\n')
-        print(cmnd)
+        inCmnd = values[event]
+
+    if event in ('_PRINT_', '_SEND_'):
+        if not ip:
+            echo('Выберите IP!', True)
+            continue
 
     if event in ('_PRINT_'):
-        if not ip:
-            print('Введите IP!')
-            continue
-
-        if not gcode:
-            print('Выберите GCODE!')
-            continue
+        if not inGcode:
+            echo('Выберите файл GCODE!', True)
         else:
-            file = re.findall('.+:\/.+\.+.', gcode)
-            if not file and file is not None:
-                print('Ошибка чтения файла!')
+            echo('', True)
+            echo('Отправка файла на принтер...', True)
+            if sendFile(inGcode):
+                time.sleep(5)
+                echo('Запуск печати...')
+                fileName = os.path.basename(inGcode)
+                if sendRawSocket(f'M23 {fileName}'):
+                    if sendRawSocket('M24'):
+                        echo(f'Печать...')
+                    else:
+                        echo('Ошибка запуска печати!')
+                else:
+                    echo('Ошибка выбора файла!')
             else:
-                print('Print')
+                echo('Ошибка отправки файла!')
 
     if event in ('_SEND_'):
-        if not ip:
-            print('Введите IP!')
-            continue
-
-        if not cmnd:
-            print('Введите команду!')
-            continue
+        if not inCmnd:
+            echo('Введите команду!', True)
         else:
-            rawSocketSend()
+            echo('', True)
+            sendRawSocket(inCmnd, True)
 
 window.close()
