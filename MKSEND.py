fd131a0dfae3a278ddfd053371fc0f88df5a303d
Пешков Александр Юрьевич <a.peshkov@ente-ltd.ru>
2020-07-21 21:31:57 +0500

    0

--- MKSEND.py
+++ MKSEND.py
@@ -1,38 +1,49 @@
+import os
 import PySimpleGUI as sg
 import re
+import requests
 import socket
 import sys
-import os
-import requests
 import time
 
 sg.ChangeLookAndFeel('LightGrey3')
 
-ip = '192.168.5.254'
-httpPort = 80
-sockPort = 8080
-inGcode = ''
-inCmnd = ''
-upItems = ['a','b','c','d']
 
-def showPopup(s):
-    sg.popup(s, no_titlebar=True, keep_on_top=True)
-
-
-def echo(s, isNew=False):
-    if isNew:
-        window['_OUTPUT_'].update(s)
-    else:
-        window['_OUTPUT_'].print(s)
+def logCls():
+    window['_OUTPUT_'].update('')
     return
 
 
-def sendRawSocket(i, isLog=False):
+def log(s):
+    window['_OUTPUT_'].print(s)
+    return
+
+
+def getIp():
+    ip = re.findall(r"(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)", values['_IP_'])
+    return ip[0] if ip else ''
+
+
+def getCmnd():
+    return values['_CMND_']
+
+
+def getGcode():
+    return values['_GCODE_']
+
+
+def isPrinted():
+    return values['_PRINT_GCODE_']
+
+
+def sendRawSocket(cmnd, ip, port=8080):
     try:
-        cmnd = str.encode(i + '\r\n')
+        result = [False]
+        bCmnd = str.encode(cmnd + '\r\n')
+        print(bCmnd)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
-            conn.connect((ip, sockPort))
-            conn.sendall(cmnd)
+            conn.connect((ip, port))
+            conn.sendall(bCmnd)
             reply = ''
             while reply != 'ok':
                 reply = ''
@@ -41,29 +52,29 @@ def sendRawSocket(i, isLog=False):
                     data = conn.recv(1)
                     reply = reply + str(data, 'utf-8')
                 reply = reply.replace("\n", "").replace("\r", "")
-                if isLog:
-                    echo(reply)
-        return True
+                result.append(reply)
+        result[0] = True
+        print(result)
     except Exception as ex:
-        echo(f'Exception: {ex}')
-        return False
+        print(f'Exception: {ex}')
     finally:
         conn.close()
         conn = None
+        return result
 
 
-def sendFile(fileUrl):
+def sendFile(file, fileName, ip, port=80):
     try:
-        with open(fileUrl, 'rb') as f:
-            fileData = f.read()
-        fileName = os.path.basename(fileUrl)
-        url = f'http://{ip}/upload?X-Filename={fileName}'
+        with open(file, 'rb') as f:
+            data = f.read()
+        url = f'http://{ip}:{port}/upload?X-Filename={fileName}'
         res = requests.post(url=url,
-                            data=fileData,
+                            data=data,
                             headers={'Content-Type': 'application/octet-stream'})
+        time.sleep(3)
         return False if res.status_code != 200 else True
     except Exception as ex:
-        echo(f'Exception: {ex}')
+        print(f'Exception: {ex}')
         return False
 
 
@@ -71,7 +82,7 @@ layout = [
     [sg.Column(layout=[
         [sg.Frame(title=' IP адрес принтера ',
                   layout=[
-                      [sg.InputText(key='_IP_', default_text=ip, size=(33, 1),
+                      [sg.InputText(key='_IP_', default_text='192.168.5.254', size=(33, 1),
                                     justification='center', background_color='white',
                                     change_submits=True, do_not_clear=True)]
                   ],
@@ -83,8 +94,8 @@ layout = [
                                     do_not_clear=True, readonly=True)],
                       [sg.FileBrowse(button_text='...', size=(3, 1), target='_GCODE_'),
                        sg.Submit(button_text='Отправить',
-                                 key='_SEND_FILE_', size=(10, 1)),
-                       sg.Checkbox(text='печать', key='_PRINT_FILE_')]
+                                 key='_SENDFILE_', size=(10, 1)),
+                       sg.Checkbox(text='печать', key='_PRINT_GCODE_')]
                   ],
                   border_width=1)
          ],
@@ -103,8 +114,15 @@ layout = [
             [sg.Frame(title=' Загруженные файлы ',
                       layout=[
                           [sg.Submit(button_text='Обновить',
-                                     key='_TREE_UPDATE_', size=(20, 1))],
-                          [sg.Listbox(values=upItems, size=(21,10), enable_events=True, bind_return_key=True, select_mode='single', key='_LISTBOX_')]
+                                     key='_TREEUPDATE_', size=(10, 1)),
+                           sg.Submit(button_text='Печать',
+                                     key='_TREEPRINT_', size=(10, 1)),
+                           sg.Submit(button_text='Удалить',
+                                     key='_TREEDELETE_', size=(10, 1)),
+                           sg.Submit(button_text='Стоп',
+                                     key='_PRINTSTOP_', size=(10, 1))],
+                          [sg.Listbox(values=[], size=(21, 10), enable_events=True,
+                                      bind_return_key=True, select_mode='single', key='_LISTBOX_')]
                       ],
                       border_width=0)
              ]
@@ -121,52 +139,82 @@ while True:
         break
 
     if event in ('_IP_'):
-        ipEntered = values[event]
-        isIp = re.findall(
-            r"(^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$)", ipEntered)
-        if isIp:
-            ip = ipEntered
+        if getIp():
             window.Element(event).update(background_color='white')
         else:
-            ip = ''
             window.Element(event).update(background_color='#ff5555')
 
-    if event in ('_GCODE_'):
-        inGcode = values[event]
-
-    if event in ('_CMND_'):
-        inCmnd = values[event]
-
-    if event in ('_SEND_FILE_', '_SEND_'):
-        if not ip:
-            showPopup('Выберите IP!')
+    if event in ('_SENDFILE_', '_SEND_'):
+        if not getIp():
+            log('Выберите IP!')
             continue
 
-    if event in ('_SEND_FILE_'):
-        if not inGcode:
-            echo('Выберите файл GCODE!', True)
+    if event in ('_SENDFILE_'):
+        gcode = getGcode()
+        if not gcode:
+            log('Выберите файл GCODE!')
         else:
-            echo('', True)
-            echo('Отправка файла на принтер...', True)
-            if sendFile(inGcode):
-                time.sleep(5)
-                echo('Запуск печати...')
-                fileName = os.path.basename(inGcode)
-                if sendRawSocket(f'M23 {fileName}'):
-                    if sendRawSocket('M24'):
-                        echo(f'Печать...')
+            log('Загрузка файла на принтер...')
+            ip = getIp()
+            fileName = os.path.basename(gcode)
+            if sendFile(gcode, fileName, ip):
+                if isPrinted():
+                    log('Запуск печати...')
+                    if sendRawSocket(f'M23 {fileName}', ip)[0]:
+                        if sendRawSocket('M24', ip)[0]:
+                            log('Печать...')
+                        else:
+                            log('Ошибка запуска печати!')
                     else:
-                        echo('Ошибка запуска печати!')
+                        log('Ошибка выбора файла!')
                 else:
-                    echo('Ошибка выбора файла!')
+                    log('Файл загружен!')
             else:
-                echo('Ошибка отправки файла!')
+                log('Ошибка отправки файла!')
 
     if event in ('_SEND_'):
-        if not inCmnd:
-            echo('Введите команду!', True)
+        cmnd = getCmnd()
+        if not cmnd:
+            log('Введите команду!')
         else:
-            echo('', True)
-            sendRawSocket(inCmnd, True)
+            log('Отправка комманды...')
+            ip = getIp()
+            res = sendRawSocket(cmnd, ip)
+            if res.pop(0):
+                logCls()
+                for s in res:
+                    log(s)
+            else:
+                log('Ошибка выполнения комманды!')
 
+    if event in ('_TREEUPDATE_'):
+        ip = getIp()
+        res = sendRawSocket('M20', ip)
+        if res.pop(0):
+            res.remove('Begin file list')
+            res.remove('End file list')
+            res.remove('ok')
+            window.Element('_LISTBOX_').update(values=res)
+
+    if event in ('_TREEPRINT_'):
+        ip = getIp()
+        selected = values['_LISTBOX_']
+        if len(selected) > 1:
+            log('Отправить на печать можно только один элемент!')
+        else:
+            for s in selected:
+                if sendRawSocket(f'M23 {s}', ip)[0]:
+                    if sendRawSocket('M24', ip)[0]:
+                        log('Печать...')
+
+    if event in ('_TREEDELETE_'):
+        ip = getIp()
+        selected = values['_LISTBOX_']
+        for s in selected:
+            if sendRawSocket(f'M30 /{s}', ip)[0]:
+                log(f'{s} удален!')
+
+    if event in ('_PRINTSTOP_'):
+        ip = getIp()
+        sendRawSocket(f'M25', ip)
 window.close()
